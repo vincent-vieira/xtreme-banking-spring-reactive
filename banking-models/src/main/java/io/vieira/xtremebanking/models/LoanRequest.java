b0a3683871dbe608a4050039ff638d1344298d6d
Vincent Vieira <vincent.vieira@carbon-it.com>
2017-05-27 13:21:05 +0200

    feat(banking-models): changes to include some data

--- banking-models/src/main/java/io/vieira/xtremebanking/models/LoanRequest.java
+++ banking-models/src/main/java/io/vieira/xtremebanking/models/LoanRequest.java
@@ -1,5 +1,39 @@
 package io.vieira.xtremebanking.models;
 
-public class LoanRequest {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotNull;
+
+public class LoanRequest implements Comparable<LoanRequest> {
+
+    @JsonCreator
+    public LoanRequest(@JsonProperty("buyer") String buyer, @JsonProperty("offer") int offer) {
+        this.buyer = buyer;
+        this.offer = offer;
+    }
+
+    @NotNull
+    private String buyer;
+
+    private int offer;
+
+    public String getBuyer() {
+        return buyer;
+    }
+
+    public int getOffer() {
+        return offer;
+    }
+
+    @Override
+    public String toString() {
+        return "LoanRequest{buyer="+ this.buyer + ", offer="+ this.offer +"}";
+    }
+
+    @Override
+    public int compareTo(LoanRequest o) {
+        if(o == null) return 1;
+        return Integer.compare(o.getOffer(), this.offer);
+    }
 }
